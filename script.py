08fdace6809f4dbf68eeee437143576fb54b8c22
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-05-16 12:28:09 -0400

    cleaner script.py

--- script.py
+++ script.py
@@ -1,18 +1,14 @@
 import os
 
-path  = r'/home/amir/Desktop/scavenger-hunt/clues/'
+path = r"/home/{USERNAME}/Desktop/scavenger-hunt/clues/"
 os.chdir(path)
 print(os.getcwd())
 list = os.listdir()
 print(list)
 for folder in list:
-    #for file in folder:
-     #   print(file)
-      #  exit()
     with open(folder+'/clue', 'r') as reader:
         for line in reader:
             if "Nothing to see" in line:
                 continue
             else:
                 print(folder)
-        #os.chdir()
